949531cdb126f482e6ce343e56ab7f0cde35425f
Sudarshana Reddy <sud8640@gmail.com>
2022-05-02 10:25:39 +0530

    stored procedure end point

---
+++ src/main/java/com/example/bookstore/controller/StoredProcedureBookCatalogue.java
@@ -0,0 +1,24 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.model.BookCatalogue;
+import com.example.bookstore.model.SaveOrderResponse;
+import com.example.bookstore.service.RetrieveBookOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1/storedproc/book")
+public class StoredProcedureBookCatalogue {
+
+    @Autowired
+    private RetrieveBookOrderService retrieveBookOrderService;
+
+    @PostMapping(path = "/catalogue", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<BookCatalogue> getBookCatalogue() {
+        return ResponseEntity.ok(retrieveBookOrderService.getAllBooksViaStoredProc());
+    }
+}
